381f7eabb753bd902e354827b5122ee6a97c87ad
Altrius <tom.b.2k2@gmail.com>
2023-08-28 14:22:20 +0100

    update sitemap generation to be absed off of the aired date of an episode

--- server/api/_sitemap-urls.ts
+++ server/api/_sitemap-urls.ts
@@ -3,8 +3,13 @@ import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (evt) => {
   const sb = await serverSupabaseClient(evt)
 
-  const episodes = (await sb.from('episodes').select('id')).data || []
+  const episodes = (await sb.from('episodes').select('*')).data || []
   return episodes.map((s: any) => {
-    return { loc: '/videos/' + s.id, lastmod: new Date() }
+    return {
+      loc: '/videos/' + s.id,
+      lastmod: new Date(s.aired),
+      changefreq: 'weekly',
+      priority: 0.8
+    }
   })
 })
