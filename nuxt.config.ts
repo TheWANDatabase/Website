8423c5689f56d8761384fd215a3db88a62675f20
Altrius <tom.b.2k2@gmail.com>
2023-09-01 12:48:17 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: process.env.SSR !== undefined || true,
+  ssr: false,
   modules: [
     '@nuxtjs/supabase',
     '@nuxtjs/color-mode',
