a595fa51f5ceab0214988c5d66d3a4a4c7eae62a
Altrius <tom.b.2k2@gmail.com>
2023-09-01 12:25:47 +0100

    update SSR config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  ssr: process.env.SSR !== undefined || true,
   modules: [
     '@nuxtjs/supabase',
     '@nuxtjs/color-mode',
