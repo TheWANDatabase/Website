784af0802962670e73a6d2e037d2fa3383293368
Altrius <tom.b.2k2@gmail.com>
2023-09-03 00:25:54 +0100

    fix linting issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-
-if(!process.env.DOMAIN) process.env.DOMAIN = process.env.VERCEL_URL
-if(!process.env.DOMAIN) process.env.DOMAIN = process.env.VERCEL_BRANCH_URL
+if (!process.env.DOMAIN) { process.env.DOMAIN = process.env.VERCEL_URL }
+if (!process.env.DOMAIN) { process.env.DOMAIN = process.env.VERCEL_BRANCH_URL }
 
 export default defineNuxtConfig({
   // ssr: false,
