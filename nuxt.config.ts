0e3297d6a0860360111f81df7010e944b31167f3
Altrius <tom.b.2k2@gmail.com>
2023-09-02 01:33:51 +0100

    re-enable SSR for dev branch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
   modules: [
     '@nuxtjs/supabase',
     '@nuxtjs/color-mode',
