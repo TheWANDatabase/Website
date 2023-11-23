ad508defb53382f23fecafe9c8a8dfb2d7299b72
Altrius <tom.b.2k2@gmail.com>
2023-11-23 09:09:31 +0000

    more experimenting
    Signed-off-by: Altrius <tom.b.2k2@gmail.com>

--- src/components/interactibles/buttons.rs
+++ src/components/interactibles/buttons.rs
@@ -3,12 +3,10 @@ use leptos::*;
 /// Renders the site header.
 #[component]
 pub fn Filled(
-    #[prop(into)]
-    click: fn(ev::MouseEvent),
     children: Children,
 ) -> impl IntoView {
     view! {
-        <button class="filled_button" on:click=click>
+        <button class="filled_button">
             {children()}
         </button>
     }
--- src/pages/__design.rs
+++ src/pages/__design.rs
@@ -1,18 +1,27 @@
 use leptos::*;
 
-use crate::components::*;
-
 /// Renders the home page of your application.
 #[component]
 pub fn DesignPreview() -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    // let on_click = |_| set_count.update(|count| *count += 1);
+
+    let on_click = move |m: MouseEvent| {
+        set_count.update(|count| *count += 1);
+        debug!("Clicked");
+        debug!("{:?}", m);
+    };
 
     view! {
         <h1>Interactibles</h1>
         <div style="display: flex">
-            <interactibles::buttons::Filled>"Click Me: " {count}</interactibles::buttons::Filled>
+            <div style="margin-right: 1rem">
+                <h2>Buttons</h2>
+                <h3>Filled</h3>
+                <div on:click=on_click>
+                    <interactibles::buttons::Filled>"Click Me: " {count}</interactibles::buttons::Filled>
+                </div>
+            </div>
         </div>
     }
 }
