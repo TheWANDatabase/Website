255a9e563b662c4e87d762a02e24d7ef1ccad8b5
Altrius <tom.b.2k2@gmail.com>
2023-11-29 07:51:59 +0000

    match docs for adapter
    Signed-off-by: Altrius <tom.b.2k2@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 
