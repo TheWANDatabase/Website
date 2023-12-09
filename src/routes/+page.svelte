3dd28f7e56d507f9ec070c01f0bc132008d143b2
Altrius <tom.b.2k2@gmail.com>
2023-12-09 01:18:10 +0000

    website work
    Signed-off-by: Altrius <tom.b.2k2@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+
+    import posthog from "posthog-js";
+    import {writable} from "svelte/store";
+    import {browser} from "$app/environment";
+
+    let show = writable(false)
+
+    if (browser) {
+        if (posthog.getFeatureFlag('landing-page-alpha')) {
+            show.set(true);
+        } else {
+            window.location.pathname = '/archive'
+        }
+    }
+
+</script>
+{#if $show}
+    <div class="container">
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+{/if}
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        width: 100%;
+        height: 100%;
+    }
+
+
+</style>
